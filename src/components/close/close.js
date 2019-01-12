f61eeeeae7ecfb6f264316efbad5324c054dc644
Alena Mitrofanova <funnymitrofunny@gmail.com>
2019-01-12 10:57:17 +0700

    temp fix for close

---
+++ src/components/close/close.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import "./close.sass";
+
+function Close(props) {
+	return <div className={`close ${props.className}`}></div>
+}
+
+export default Close;
--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,21 +1,43 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-const IMAGES = [
-  { id: 0, title: "Dark Orchid", color: "DarkOrchid" },
-  { id: 1, title: "Lime Green", color: "LimeGreen" },
-  { id: 2, title: "Tomato", color: "Tomato" },
-  { id: 3, title: "Seven Ate Nine", color: "#789" },
-  { id: 4, title: "Crimson", color: "Crimson" }
-];
+import "./modal.sass";
+import Close from "../close/close.js";
+import "../close/close.sass";
 
 class Modal extends Component {
 	constructor(props) {
     super(props);
-    this.state = {data: []};
+    this.state = {
+			error: null,
+			isLoaded: false,
+			data: [],
+		};
   }
+
+	componentDidMount() {
+		const idParam = this.props.match.params.id;
+		fetch(`https://biotax-api.herokuapp.com/api/taxon/${idParam}`)
+			.then(res => res.json())
+			.then(
+				(result) => {
+					console.log("fetch taxon... ", result);
+					this.setState({
+						isLoaded: true,
+						data: result
+					});
+				},
+				(error) => {
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				}
+			)
+	}
+
 	render() {
-		let taxonId = this.props.match.params.id;
+		const { error, isLoaded, data } = this.state;
+		const taxonId = this.props.match.params.id;
 
 		if (!taxonId) return null;
 
@@ -24,39 +46,56 @@ class Modal extends Component {
 			this.props.history.goBack();
 		};
 
-		return (
-			<div
-				onClick={back}
-				style={{
-					position: "absolute",
-					top: 0,
-					left: 0,
-					bottom: 0,
-					right: 0,
-					background: "rgba(0, 0, 0, 0.15)"
-				}}
-			>
-				<div
-					className="modal"
-					style={{
-						position: "absolute",
-						background: "#fff",
-						top: 25,
-						left: "10%",
-						right: "10%",
-						padding: 15,
-						border: "2px solid #444"
-					}}
-				>
-					<h1>{taxonId}</h1>
-					<button type="button" onClick={back}>
-						Close
-					</button>
-				</div>
-			</div>
-		);
-	}
 
+		if (error) {
+			return <div>Error: {error.message}</div>;
+		} else if (!isLoaded) {
+			return <div>Loading...</div>;
+		} else {
+			return (
+				<div className="modal" data-id={data.id}>
+					<div className="modal__bg"></div>
+
+					<div className="modal__inner">
+						<div className="modal__title"> {data.title} </div>
+
+						<div className="modal__content">
+							<div className="modal__hierarchy"></div>
+
+							<div className="modal__info">
+							<img src={`${data.image_url}`} alt={`${data.name}`} className="modal__img" />
+
+								<div className="modal__description">
+									{data.description}
+								</div>
+							</div>
+						</div>
+
+						<div className="modal__controls">
+							<a href={`${data.url}`} target="_blank" className="modal__control-btn">
+								<img src="assets/images/wiki-icon.svg" alt="read on wikipedia" />
+								Read more
+							</a>
+
+							<a href="" target="_blank" className="modal__control-btn">
+								Share
+							</a>
+
+							<div className="modal__control-btn" id="show-hierarchy">
+								Close modal and show hierarchy
+							</div>
+
+							<div className="modal__control-btn" id="add-bookmark">
+								Bookmark
+							</div>
+						</div>
+
+						<div className="close modal__close" onClick={back}></div>
+					</div>
+				</div>
+			);
+		}
+	}
 }
 
 export default Modal;
