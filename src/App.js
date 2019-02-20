c56ce6c6bb714950777c40ab613dd6d697a82faf
Alena Mitrofanova <funnymitrofunny@gmail.com>
2019-02-20 16:59:04 +0500

    cont add home

--- src/App.js
+++ src/App.js
@@ -104,40 +104,61 @@ class Tree extends Component {
     this.props.history.push(`?${url}`);
   };
 
-  searchChildren = query => {
+  paintTree = query => {
     if (!query) {
-      return this.setState({
-        results: []
-      });
+      // return this.setState({
+      //   results: []
+      // });
+      fetch("https://biotax-api.herokuapp.com/api/kingdoms")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            console.log("kingdoms res: ", result);
+
+            this.setState({
+              // isLoaded: true,
+              // rows: [{rank: "Kingdom", items: result}],
+              loading: false,
+              results: result
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    } else {
+      this.setState({ loading: true, error: false });
+      fetch(`https://biotax-api.herokuapp.com/api/children/${query}`)
+        .then(res => res.json())
+        .then((data) => {
+          console.log("query: ", query);
+          console.log("fetching... ", data);
+          this.setState({
+            results: data.children,
+            loading: false
+          })
+        }
+        )
+        .catch(e => this.setState({ loading: false, error: true }));
+
+        const url = setParams({ query: this.state.queryString });
+        console.log("queryString: ", this.state.queryString);
+        this.props.history.push(`?${url}`);
     }
-
-    this.setState({ loading: true, error: false });
-    fetch(`https://biotax-api.herokuapp.com/api/children/${query}`)
-      .then(res => res.json())
-      .then((data) => {
-        console.log("query: ", query);
-        console.log("fetching... ", data);
-        this.setState({
-          results: data.children,
-          loading: false
-        })
-      }
-      )
-      .catch(e => this.setState({ loading: false, error: true }));
-
-      const url = setParams({ query: this.state.queryString });
-      console.log("queryString: ", this.state.queryString);
-      this.props.history.push(`?${url}`);
-
   };
 
   componentDidMount() {
-    return this.searchChildren(this.props.query);
+    console.log("compDidMount props.query: ", this.props.query);
+    return this.paintTree(this.props.query);
   }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.query !== this.props.query) {
       this.setState({ query: nextProps.query });
-      return this.searchChildren(nextProps.query);
+      return this.paintTree(nextProps.query);
     }
   }
 
@@ -156,20 +177,44 @@ class Tree extends Component {
             type="button"
             className="button"
             value="Search"
-            onClick={() => this.searchChildren(this.state.queryString)}
+            onClick={() => this.paintTree(this.state.queryString)}
           />
         </div>
         <br/>
         <div className="loading">Loading state: {this.state.loading.toString()} </div>
         <div className="error">Errror state: {this.state.error.toString()} </div>
+
         <div>
-          {this.state.results.map(child => (
-            <div className="child" key={child.id}>
-              <a className="child-link">
-                {child.name}
-              </a>
-              <div className="child-owner">{`by ${child.rank}`}</div>
-            </div>
+          {this.state.results.map(card => (
+            <div className="card" key={card.id}>
+      				<div className="card__inner">
+      					<div className="card__title">
+      						{card.title}
+      					</div>
+
+      					<div className="card__img">
+      						<img src="https://via.placeholder.com/150" alt="testimg" />
+      					</div>
+
+      					<div className="card__description">
+      						{card.description} description
+      					</div>
+      				</div>
+
+      				<div className="card__controllers">
+      					<div className="card__wiki-btn">
+      						<img src="https://via.placeholder.com/15" alt="read on wikipedia" />
+      					</div>
+
+      					<div
+      						data-id={card.id}
+      						className="card__hierarchy-btn"
+      						onClick={() => this.paintTree(this.state.queryString)}
+      					>
+      						<img src="https://via.placeholder.com/15" alt="hierarchy" />
+      					</div>
+      				</div>
+      			</div>
           ))}
         </div>
       </React.Fragment>
@@ -189,12 +234,7 @@ class App extends Component {
               render={({ location, history }) => {
                 const { query } = getParams(location);
 
-                if (query) {
-                  return <MainPage query={query} history={history} />;
-                } else {
-                  return <Home />
-                }
-
+                return <MainPage query={query} history={history} />;
               }}
             />
 					</Switch>
