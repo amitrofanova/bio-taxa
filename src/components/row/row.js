b0f60469bab85573b560898c8f57aa15c34d90e0
Alena Mitrofanova <funnymitrofunny@gmail.com>
2019-03-12 13:55:55 +0500

    add ranks

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -5,19 +5,23 @@ import "./row.sass";
 
 function Row(props) {
 	return (
-		<div className="row" data-rank={props.rank}>
-			{props.data.map(i => (
-				<Card
-					key={i.id}
-					id={i.id}
-					name={i.name}
-					image={i.image_url}
-					title={i.title}
-					description={i.description}
-					row={props.row}
-					onClick={props.onClick}
-				/>
-			))}
+		<div className="row">
+			<div className="row__rank">{props.rank}</div>
+
+			<div className="row__cards" data-rank={props.rank}>
+				{props.data.map(i => (
+					<Card
+						key={i.id}
+						id={i.id}
+						name={i.name}
+						image={i.image_url}
+						title={i.title}
+						description={i.description}
+						row={props.row}
+						onClick={props.onClick}
+					/>
+				))}
+			</div>
 		</div>
 	)
 }
